cc325799df6b851f84b0b9579b0ae9025eed5f4e
Dino Iván Pérez Vásquez <55163683+Dinoelfantastico@users.noreply.github.com>
2020-09-30 08:45:44 -0500

    Session
    Creación de la clase Session

---
+++ EasyNutrition.API/EasyNutrition.API/Domain/Models/Session.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyNutrition.API.Domain.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        public string Start { get; set; }
+
+        public string End { get; set; }
+
+        public string Link { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ EasyNutrition.API/EasyNutrition.API/Domain/Models/SessionDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyNutrition.API.Domain.Models
+{
+    public class SessionDetail
+    {
+
+        public int Id { get; set; }
+
+        public string State { get; set; }
+
+        public int SessionId { get; set; }
+
+        public Session Session { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        public IList<Session> Sessions { get; set; } = new List<Session>();
+    }
+}
