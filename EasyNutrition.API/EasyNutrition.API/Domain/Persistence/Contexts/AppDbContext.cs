9f4f8fe381aaf703e154c7d1e8afe9a1beee3e57
JoseFloresSerpa <44204029+JoseFloresSerpa@users.noreply.github.com>
2020-09-30 14:25:20 -0500

    Prueba 2
    Prueba 2

--- EasyNutrition.API/EasyNutrition.API/Domain/Persistence/Contexts/AppDbContext.cs
+++ EasyNutrition.API/EasyNutrition.API/Domain/Persistence/Contexts/AppDbContext.cs
@@ -42,7 +42,9 @@ namespace EasyNutrition.API.Domain.Persistence.Contexts
                 (
                     new Role { Id = 1, Name = "Nutricionista" },
                     new Role { Id = 2, Name = "Cliente" },
-                    new Role { Id = 3, Name = "Administrador" }
+                    new Role { Id = 3, Name = "Administrador" },
+                    new Role { Id = 4, Name = "Administrador" }
+
 
                 );
 
