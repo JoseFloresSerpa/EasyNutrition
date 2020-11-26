0260627b9d182a3dabee88e774e84b61eaebcbab
rodrities <54970340+rodrities@users.noreply.github.com>
2020-11-26 13:18:11 -0500

    Add files via upload

---
+++ EasyNutrition.API/EasyNutrition.API.Test/Features/FindDiets.feature
@@ -0,0 +1,10 @@
+﻿Feature: FindDiets
+	In order to find my diets
+	As a user customer
+	I want to see all mi diets
+
+@mytag
+Scenario: Find diets
+	Given the id of the user 
+	When the id is 5
+	Then the result should be an empty list
---
+++ EasyNutrition.API/EasyNutrition.API.Test/Steps/FindDietsSteps.cs
@@ -0,0 +1,53 @@
+﻿using EasyNutrition.API.Domain.Models;
+using FluentAssertions;
+using System;
+using TechTalk.SpecFlow;
+using EasyNutrition.API.Domain.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using EasyNutrition.API.Services;
+using EasyNutrition.API.Domain.Services.Communication;
+
+namespace EasyNutrition.API.Test.Steps
+{
+    [Binding]
+    public class FindDietsSteps
+    {
+        [Given(@"the id of the user")]
+        public void GivenTheIdOfTheUser()
+        {
+        }
+        
+        [When(@"the id is (.*)")]
+        public void WhenTheIdIs(int id)
+        {
+            id.Should().Be(5);
+        }
+
+        [Then(@"the result should be a empty list")]
+        public async void ThenTheResultShouldBeAEmptyList()
+        {
+            var mockDietRepository = GetDefaultIDietRepositoryInstance();
+            mockDietRepository.Setup(r => r.ListAsync())
+                .ReturnsAsync(new List<Diet>());
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var service = new DietService(
+                mockDietRepository.Object,
+                mockUnitOfWork.Object);
+            List<Diet> result = (List<Diet>)await service.ListAsync();
+            int dietsCount = result.Count;
+            dietsCount.Should().Equals(0);
+        }
+        private Mock<IDietRepository> GetDefaultIDietRepositoryInstance()
+        {
+            return new Mock<IDietRepository>();
+        }
+
+        private Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+    }
+}
